c5207b436a6a01149b9431ce849074a83d407384
Ashish Kashyap <akk312000@gmail.com>
2022-04-20 15:45:45 +0530

    added text in the main page

--- dollop/src/Components/Hero.jsx
+++ dollop/src/Components/Hero.jsx
@@ -1,16 +1,25 @@
-import { Container,Text} from '@chakra-ui/react'
-
+import { Container, Text, Box } from "@chakra-ui/react";
 
 function Hero() {
-  return <div>
-<Container centerContent >
-<Text fontSize="20" wordSpacing="20px"  paddingBottom="100"   px={0} mx={0}>
-    
-  There are many benefits to a joint design and develdfdfdfdfdf opment system. Not only does it bring benefits to the design team, but it also brings benefits to
-  engineering teams. It makes sure that our experiences have a consistent look and feel, not just in our design specs, but in production
-  </Text>
-</Container>
-  </div>;
+  return (
+    <Container centerContent>
+      <Box width="900px">
+        <Text
+          fontSize="20"
+          wordSpacing="20px"
+          paddingBottom="100"
+          px={0}
+          mt="-200px"
+        >
+          There are many benefits to a joint design and develdfdfdfdfdf opment
+          system. Not only does it bring benefits to the design team, but it
+          also brings benefits to engineering teams. It makes sure that our
+          experiences have a consistent look and feel, not just in our design
+          specs, but in production
+        </Text>
+      </Box>
+    </Container>
+  );
 }
 
 export default Hero;
--- dollop/src/Components/Navbar.jsx
+++ dollop/src/Components/Navbar.jsx
@@ -40,7 +40,7 @@ export default function Nav() {
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} marginLeft={0}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Box>Typing Steel</Box>
+          <Box>Typing Impactor</Box>
 
           <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
